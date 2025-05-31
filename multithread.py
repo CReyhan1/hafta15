1070fb48749bdf53c44604ea50509bb298532355
CReyhan1 <reyhanc.335@gmail.com>
2025-05-31 13:58:55 +0300

    Update multithread.py

--- multithread.py
+++ multithread.py
@@ -1,13 +1,13 @@
 import threading
 
-def coder(number,number1,number2,number3):
-    print(f'Coder ID: {number},{number1},{number2},{number3}')
+def coder(number,number1):
+    print(f'Coder ID: {number},{number1}')
 
 threads = []
 for k in range(5):
     for a in range(5):
-        for b in range(5):
-            for c in range(5):
-                t = threading.Thread(target=coder, args=(k, a,b,c))
-                threads.append(t)
-                t.start()
+        t = threading.Thread(target=coder, args=(k,a))
+        threads.append(t)
+        t.start()
+       
+               
